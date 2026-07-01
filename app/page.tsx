21d5d47b0ffc57b5c32bd2e633790eaf67541c43
murice0607-crypto <murice0607@gmail.com>
2026-07-01 13:25:57 +0900

    Refactor Home component layout and styles
    Updated the opacity of the background image and improved the heading style.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-<main className="relative h-screen flex items-center justify-center bg-black">
+export default function Home() {
+  return (
+    <main className="relative h-screen flex items-center justify-center bg-black">
 
-  <img
-    src="/hero.jpg"
-    className="absolute inset-0 w-full h-full object-cover opacity-40"
-  />
+      <img
+        src="/hero.jpg"
+        className="absolute inset-0 w-full h-full object-cover opacity-50"
+      />
 
-  <div className="relative z-10 text-center">
-    <h1>SALES GUILD</h1>
-  </div>
+      <div className="relative z-10 text-center">
+        <h1 className="text-7xl font-black text-yellow-400">
+          SALES GUILD
+        </h1>
+      </div>
 
-</main>
+    </main>
+  );
+}
